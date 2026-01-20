e55a0cbc018e0f149894616841cc5a3da924c852
valentin-grisel <valentin.grisel@epitech.eu>
2026-01-20 15:36:32 -0500

    Update page.js

--- app/forgot-password/page.js
+++ app/forgot-password/page.js
@@ -32,7 +32,7 @@ export default function ForgotPasswordPage() {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ email }),
+        body: JSON.stringify(email),
       });
 
       const data = await response.json();
